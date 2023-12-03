8c21a8e037ace88ce445245738c35855cab4a05e
Alex Meyer <25089451+meyerche@users.noreply.github.com>
2023-12-02 23:45:23 -0500

    tweeked names in day 1

--- day/day1.go
+++ day/day1.go
@@ -1,35 +1,33 @@
 package day
 
 import (
-	"fmt"
-	"unicode"
 	"strings"
+	"unicode"
 )
 
 type calibrationValue struct {
 	firstValue string
 	firstIndex int
-	lastValue string
-	lastIndex int
+	lastValue  string
+	lastIndex  int
 }
 
-func Day1Part1(data []string) int {
+func Day1Part1(lines []string) int {
 	calibration := 0
-	for _, value := range data {
-		calibration += (findFirstDigit(value) * 10) + findLastDigit(value)
+	for _, line := range lines {
+		calibration += (findFirstDigit(line) * 10) + findLastDigit(line)
 	}
-	
+
 	return calibration
 }
 
-func Day1Part2(data []string) int {
+func Day1Part2(lines []string) int {
 	calibration := 0
-	for _, value := range data {
-		newValue := convertNumbers(value)
-		fmt.Println(newValue)
-		calibration += (findFirstDigit(newValue) * 10) + findLastDigit(newValue)
+	for _, line := range lines {
+		convertedLine := convertNumbers(line)
+		calibration += (findFirstDigit(convertedLine) * 10) + findLastDigit(convertedLine)
 	}
-	
+
 	return calibration
 }
 
@@ -44,8 +42,8 @@ func findFirstDigit(word string) int {
 
 func findLastDigit(word string) int {
 	runeWord := []rune(word)
-	
-	for i := len(runeWord)-1; i >= 0; i-- {
+
+	for i := len(runeWord) - 1; i >= 0; i-- {
 		if unicode.IsDigit(runeWord[i]) {
 			return int(runeWord[i] - '0')
 		}
@@ -55,37 +53,38 @@ func findLastDigit(word string) int {
 
 func convertNumbers(word string) string {
 	numbers := map[string]string{
-		"zero": "0",
-		"one": "1",
-		"two": "2",
+		"zero":  "0",
+		"one":   "1",
+		"two":   "2",
 		"three": "3",
-		"four": "4",
-		"five": "5",
-		"six": "6",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
 		"seven": "7",
 		"eight": "8",
-		"nine": "9"}
-	
+		"nine":  "9",
+	}
+
 	var idx int
-	value := calibrationValue{"0", len(word)+1, "0", -1}
-	
+	value := calibrationValue{"0", len(word) + 1, "0", -1}
+
 	for key, val := range numbers {
 		idx = strings.Index(word, key)
-		
+
 		if idx >= 0 && idx < value.firstIndex {
 			value.firstValue = val
 			value.firstIndex = idx
 		}
-		
+
 		idx = strings.LastIndex(word, key)
-		
+
 		if idx >= 0 && idx > value.lastIndex {
 			value.lastValue = val
 			value.lastIndex = idx
 		}
 	}
-	
-	if value.lastIndex >=0 {
+
+	if value.lastIndex >= 0 {
 		word = word[:value.lastIndex] + value.lastValue + word[value.lastIndex:]
 	}
 	if value.firstIndex <= len(word) {
